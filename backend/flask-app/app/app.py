fca111f830f588bc1d25f431d89e764d78f85631
nikitosik322 <nikitosik123456789@internet.ru>
2024-05-19 07:46:34 +0500

    Update app.py

--- backend/flask-app/app/app.py
+++ backend/flask-app/app/app.py
@@ -2,9 +2,9 @@ from config import Config
 #from flask_migrate import Migrate
 from routes import app, db
 
-app.config.from_object(Config)
+#app.config.from_object(Config)
 #migrate = Migrate(app, db)
 
 db.init_app(app)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000)
